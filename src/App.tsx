0dad98278e6cadeade63fa23d3e7313a70b06b20
Lee Yong Quan <yq.lee@quandatics.com>
2026-04-14 17:11:10 +0800

    log

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ import { MyRequests } from './screens/MyRequests'
 const msalInstance = new PublicClientApplication(msalConfig)
 
 // ─── Login Page ────────────────────────────────────────────────────────────
+console.log("ENV DEBUG:");
+console.log("TENANT ID:", process.env.REACT_APP_AZURE_TENANT_ID);
+console.log("CLIENT ID:", process.env.REACT_APP_AZURE_CLIENT_ID);
+console.log("SITE URL:", process.env.REACT_APP_SHAREPOINT_SITE_URL);
+console.log("SITE ID:", process.env.REACT_APP_SHAREPOINT_SITE_ID);
+console.log("FLOW URL:", process.env.REACT_APP_FLOW_SUBMIT_URL);
 
 function LoginPage() {
   const { instance } = useMsal()
