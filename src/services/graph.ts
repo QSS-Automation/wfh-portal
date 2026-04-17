b26fc09879c97af03465288388120b584745b5db
Lee Yong Quan <yq.lee@quandatics.com>
2026-04-17 14:15:10 +0800

    final 8.5

--- src/services/graph.ts
+++ src/services/graph.ts
@@ -145,15 +145,15 @@ export async function fetchProjects(
       projectName: item.fields.ProjectName ?? '',
 
       projectManagerEmail: item.fields.ProjectManagerEmail ?? '',
-      projectManagerName: await getUserName(item.fields.ProjectManagerEmail),
+      projectManagerName: await getUserName(msal, item.fields.ProjectManagerEmail),
 
       techLeadEmail: item.fields.TechLeadEmail ?? '',
-      techLeadName: await getUserName(item.fields.TechLeadEmail),
+      techLeadName: await getUserName(msal, item.fields.TechLeadEmail),
 
       ctoEmail: item.fields.CTOEmail ?? '',
       ctoName:
         item.fields.CTO?.LookupValue ??
-        (await getUserName(item.fields.CTOEmail)),
+        (await getUserName(msal, item.fields.CTOEmail)),
 
       isActive: item.fields.IsActive ?? true,
     }))
